1e1e6c7078117e2db0e348d1722b42fbc2695ef1
Shun Usami <usatie@gmail.com>
2023-11-05 22:25:56 +0900

    Handle Prisma exceptions with the nestjs-prisma package

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
