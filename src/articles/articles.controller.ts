99bb74b68e2f9341a1c8cd8fbc2bbd9bfd3b6a30
Shun Usami <usatie@gmail.com>
2023-11-05 22:02:37 +0900

    Detect and throw exceptions directly

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  NotFoundException,
   ParseIntPipe,
 } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
@@ -39,8 +40,12 @@ export class ArticlesController {
 
   @Get(':id')
   @ApiOkResponse({ type: ArticleEntity })
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.articlesService.findOne(id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const article = await this.articlesService.findOne(id);
+	if (!article) {
+		throw new NotFoundException(`Article with ${id} does not exist.`);
+	}
+	return article;
   }
 
   @Patch(':id')
