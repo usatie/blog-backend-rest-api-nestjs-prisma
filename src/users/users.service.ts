52cde492aaf27d1185e0e51fbb0f61cef2a61753
Shun Usami <usatie@gmail.com>
2023-11-06 14:40:16 +0900

    Add PrismaClient to the Users module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
